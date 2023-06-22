29415f7393f32129d3a6b50c4ad7f0bfa9dfad18
balogun Lukman <lukheebalo@gmail.com>
2023-06-22 23:22:23 +0100

    Terraform Hot fix

--- terraform/eks_infrastructure/outputs.tf
+++ terraform/eks_infrastructure/outputs.tf
@@ -3,18 +3,18 @@
 
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.education.address
+  value       = aws_db_instance.housePrice.address
   sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.education.port
+  value       = aws_db_instance.housePrice.port
   sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.education.username
+  value       = aws_db_instance.housePrice.username
   sensitive   = true
 }
