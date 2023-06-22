c73165cb7d7d71390d02cd578fd1068334054a03
balogun Lukman <lukheebalo@gmail.com>
2023-06-22 23:37:26 +0100

    Terraform Hot fix

--- terraform/eks_infrastructure/outputs.tf
+++ terraform/eks_infrastructure/outputs.tf
@@ -3,18 +3,18 @@
 
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.housePrice.address
+  value       = aws_db_instance.house_price.address
   sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.housePrice.port
+  value       = aws_db_instance.house_price.port
   sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.housePrice.username
+  value       = aws_db_instance.house_price.username
   sensitive   = true
 }
--- terraform/eks_infrastructure/rds.tf
+++ terraform/eks_infrastructure/rds.tf
@@ -7,7 +7,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
 
-  name                 = "housePrice"
+  name                 = "house_price"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
@@ -15,17 +15,17 @@ module "vpc" {
   enable_dns_support   = true
 }
 
-resource "aws_db_subnet_group" "housePrice" {
-  name       = "housePrice"
+resource "aws_db_subnet_group" "house_price" {
+  name       = "house_price"
   subnet_ids = module.vpc.public_subnets
 
   tags = {
-    Name = "housePrice"
+    Name = "house_price"
   }
 }
 
 resource "aws_security_group" "rds" {
-  name   = "housePrice_rds"
+  name   = "house_price_rds"
   vpc_id = module.vpc.vpc_id
 
   ingress {
@@ -43,12 +43,12 @@ resource "aws_security_group" "rds" {
   }
 
   tags = {
-    Name = "housePrice_rds"
+    Name = "house_price_rds"
   }
 }
 
-resource "aws_db_parameter_group" "housePrice" {
-  name   = "housePrice"
+resource "aws_db_parameter_group" "house_price" {
+  name   = "house_price"
   family = "postgres14"
 
   parameter {
@@ -57,17 +57,17 @@ resource "aws_db_parameter_group" "housePrice" {
   }
 }
 
-resource "aws_db_instance" "housePrice" {
-  identifier             = "housePrice"
+resource "aws_db_instance" "house_price" {
+  identifier             = "house_price"
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
   engine_version         = "14.1"
-  username               = "housePrice"
+  username               = "house_price"
   password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.housePrice.name
+  db_subnet_group_name   = aws_db_subnet_group.house_price.name
   vpc_security_group_ids = [aws_security_group.rds.id]
-  parameter_group_name   = aws_db_parameter_group.housePrice.name
+  parameter_group_name   = aws_db_parameter_group.house_price.name
   publicly_accessible    = true
   skip_final_snapshot    = true
 }
