940bf7c39269211dd8f212dab4d38611ec7d163c
Balogun Lukman O <o.balogun@ymail.com>
2023-06-30 01:25:29 +0100

    Test branch (#5)
    * Destroy cluster
    
    * Hot Fix
    
    * Chore: ML model test
    
    * Doc: Update project digram
    
    * Doc: Update project digram
    
    * Doc: Update project digram
    
    * Workflow: rds infrastructure deployment
    
    * Workflow: rds infrastructure deployment
    
    * Workflow: rds infrastructure deployment
    
    * Workflow: rds infrastructure deployment
    
    * Terraform Hot fix
    
    * Terraform Hot fix
    
    * Terraform Hot fix
    
    * Terraform Hot fix
    
    * Terraform Hot fix
    
    * Terraform rds fix
    
    * Terraform rds fix
    
    * Workflow: delete resources
    
    * Workflow: delete resources
    
    * Chore: Terraform resources validation
    
    * Chore: Project description
    
    * Makefile: testing command update
    
    * Chore: application deployment
    
    * Chore: deploy application
    
    * Chore: deploy application
    
    * chore: preview application url
    
    * chore: preview application url
    
    * chore: preview application url
    
    * Chore: update deployment with version
    
    * Chore: update deployment with version
    
    * Doc: updated infrastructure diagram
    
    * Chore: destroy infrastructure
    
    * Chore: deploy infrastructure
    
    * Chore: infrastructure deployment
    
    * Chore: app deployment to eks
    
    * Chore: update infrastructure
    
    * Chore: update application
    
    * quick fix
    
    * hot fix
    
    * Hot fix
    
    ---------
    
    Co-authored-by: balogun Lukman <lukheebalo@gmail.com>

---
+++ terraform/eks_infrastructure/outputs.tf
@@ -0,0 +1,20 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.house_price.address
+  # sensitive   = true
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.house_price.port
+  # sensitive   = true
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.house_price.username
+  sensitive   = true
+}
---
+++ terraform/eks_infrastructure/rds.tf
@@ -0,0 +1,73 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name                 = "house_price"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_db_subnet_group" "house_price" {
+  name       = "house_price"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "house_price"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name   = "house_price_rds"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "house_price_rds"
+  }
+}
+
+resource "aws_db_parameter_group" "house_price" {
+  name   = "houseprice"
+  family = "postgres14"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
+resource "aws_db_instance" "house_price" {
+  identifier             = "houseprice"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.3"
+  username               = "house_price"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.house_price.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.house_price.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
